a4f096eee71b2f6ace27354c1f7c900a993bcbef
lgcastro710 <gerardocastro710@gmail.com>
2026-06-11 21:02:02 -0300

    actualizando router

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/!* webpackChunkName: "about" *!/ '../views/Store.vue')
   }*/
+ 
 ]
 
 const router = new VueRouter({
